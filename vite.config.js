cb18b2488cb611c39b86c57b506683a862095163
root <root@MyMSI>
2024-07-27 10:04:01 -0600

    updated base in vite.config.js to deploy at GH pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/weather-api/', // Replace 'my-project' with your GitHub repo name  
 })
